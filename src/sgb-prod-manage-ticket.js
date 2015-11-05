803683a6d99e905d918b90aba6b9c9ad12c8bc51
Maria Rodriguez <maria.rodriguez@synergy-gb.com>
2015-11-05 12:02:37 -0430

    Agregados traducciones y ejemplo de timer

--- src/sgb-prod-manage-ticket.js
+++ src/sgb-prod-manage-ticket.js
@@ -4,7 +4,8 @@ angular.module('sgb-prod-manage-ticket', ['megazord'])
     .controller('sgb-prod-manage-ticket-controller', ['$scope', '_router', '_screen', '_screenParams', '_data', '$ionicGesture', '$window', '$interval', function ($scope, _router, _screen, _screenParams, _data, $ionicGesture, $window, $interval) {
         _screen.initialize($scope, _screenParams);
 
-        $scope.prueba = '';
+        $scope.data = _data;
+
         $scope.ticketGiven=false;
 
         var element = angular.element(document.getElementById('eventPlaceholder'));
@@ -13,4 +14,48 @@ angular.module('sgb-prod-manage-ticket', ['megazord'])
         		$scope.ticketGiven=true;
         	});
         }, element);
+
+        //Probando el timer
+
+        /*var defaults = {}
+		  , one_second = 1000
+		  , one_minute = one_second * 60
+		  , one_hour = one_minute * 60
+		  , one_day = one_hour * 24
+		  , startDate = new Date()
+		  , face = document.getElementById('lazy');
+
+		// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+		var requestAnimationFrame = (function() {
+		  return window.requestAnimationFrame       || 
+		         window.webkitRequestAnimationFrame || 
+		         window.mozRequestAnimationFrame    || 
+		         window.oRequestAnimationFrame      || 
+		         window.msRequestAnimationFrame     || 
+		         function( callback ){
+		           window.setTimeout(callback, 1000 / 60);
+		         };
+		}());
+
+		tick();
+
+		function tick() {
+
+		  var now = new Date()
+		    , elapsed = now - startDate
+		    , parts = [];
+
+		  parts[0] = '' + Math.floor( elapsed / one_hour );
+		  parts[1] = '' + Math.floor( (elapsed % one_hour) / one_minute );
+		  parts[2] = '' + Math.floor( ( (elapsed % one_hour) % one_minute ) / one_second );
+
+		  parts[0] = (parts[0].length == 1) ? '0' + parts[0] : parts[0];
+		  parts[1] = (parts[1].length == 1) ? '0' + parts[1] : parts[1];
+		  parts[2] = (parts[2].length == 1) ? '0' + parts[2] : parts[2];
+
+		  face.innerText = parts.join(':');
+		  
+		  requestAnimationFrame(tick);
+		  
+		}*/
     }]);
